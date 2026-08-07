c776422a4e7daaaa001888378beb0ee3459e1af0
Ivan <ivan@quad4.io>
2026-08-07 00:47:16 -0500

    Update Docker entrypoint

--- dockerized/vintage-story/docker-entrypoint.sh
+++ dockerized/vintage-story/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -eu
 
+if [ "$(id -u)" = "0" ]; then
+    mkdir -p "${VS_ROOT}" "${VS_SERVER_DIR}" "${VS_DATA_DIR}"
+    chown -R vintagestory:vintagestory "${VS_ROOT}" /home/vintagestory 2>/dev/null || true
+    exec runuser -u vintagestory -- /bin/bash /home/vintagestory/entrypoint.sh "$@"
+fi
+
 mkdir -p "${VS_ROOT}" "${VS_SERVER_DIR}" "${VS_DATA_DIR}"
 exec /bin/bash /home/vintagestory/entrypoint.sh "$@"
